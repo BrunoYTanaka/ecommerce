7169a720fedb8a369879c7e422b73840992db538
Bruno <brunoyoichi@hotmail.com>
2018-07-16 08:43:41 -0400

    admin-categorias

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin; 
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 
 $app = new \Slim\Slim();
@@ -247,10 +248,110 @@ $app->post("/admin/forgot/reset",function(){
 
 	$page->setTpl("forgot-reset-success");
 
+});
 
+
+$app->get("/admin/categories",function (){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories",[
+		"categories" =>$categories
+	]);
 
 
 });
+
+/*Página de criar nova categoria*/
+$app->get("/admin/categories/create",function (){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+
+});
+
+/*Criar nova categoria*/
+
+$app->post("/admin/categories/create",function (){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+
+	exit;
+});
+
+
+/*Deletar uma categoria*/
+$app->get("/admin/categories/:idcategory/delete",function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
+});
+
+
+/*Editar categoria*/
+$app->get("/admin/categories/:idcategory",function ($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update",array(
+		"category" => $category->getValues()
+	));
+
+	exit;
+
+});
+
+$app->post("/admin/categories/:idcategory",function ($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+
+
+	exit;
+
+});
+
+
+
 
 $app->run();
 
